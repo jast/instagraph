4c887a458e16a25f13f1ea2ab94d13deeb0fa69f
Jan Krüger <jk@jk.gs>
2010-06-02 20:03:02 +0200

    Add reference to syntax and provide example graph
    To make the pastebin form slightly more user-friendly, link to the
    Wikipedia article describing the DOT syntax, and make an example graph
    definition available. Also very briefly explain what the pastebin page
    is all about and what the rules are.

--- index.php
+++ index.php
@@ -3,14 +3,25 @@
 	<title>Graph generator thingy</title>
 </head><body>
 <h1>Graph generator</h1>
+
+<p>Here you can generate your own graphs. Generated graphs will be stored
+indefinitely and the generated image URLs will not change. Once generated, a
+graph cannot be changed, but you can generate a new one.
+
 <form method="post" action="gen.php">
 <p>Type: <input type="radio" name="type" value="graph" id="type_g"> <label
 for="type_g">undirected</label> or <input type="radio" name="type"
 value="digraph" id="type_d" checked="checked">
 <label for="type_d">directed</label> graph
 
-<p>Definition (contents of a dot graph {} container):<br>
-<textarea name="def" rows="20" cols="50"></textarea>
+<p>Definition (without the surrounding <code>[di]graph {}</code> thing): (<a
+href="http://en.wikipedia.org/wiki/DOT_language">syntax</a>, <a
+href="index.php?example=1">example</a>)<br>
+<textarea name="def" rows="20" cols="50"><?php if ($_GET['example']):
+?>Sleep -&gt; Work [label="zombie"]
+Work -&gt; TV [label="zombie"]
+TV -&gt; Sleep [label="zombie"]
+<?php endif; ?></textarea>
 
 <p><input type="checkbox" name="private" value="1" id="priv"> <label
 for="priv">private (non-guessable URL)</label>
